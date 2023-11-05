d79559bafacaf875c6dc825a3e11b7c5ed46952a
Peter Heiss <peter.heiss@uni-muenster.de>
2023-11-05 15:49:47 +0100

    Update src/todoistSyncAPI.ts
    If the judgment conditions in the latter part are added, it will result in some tasks created in Obsidian that cannot be synchronized back to Obsidian after being modified in Todoist.
    
    Co-authored-by: HeroBlackInk <drv1@outlook.com>

--- src/todoistSyncAPI.ts
+++ src/todoistSyncAPI.ts
@@ -165,7 +165,7 @@ export class TodoistSyncAPI   {
         //console.log(allActivity)
         const allActivityEvents = allActivity.events
         //client中不包含obsidian 的activity
-        const filteredArray = allActivityEvents.filter(obj => !obj.extra_data.client?.includes("obsidian") && this.plugin.settings.pullFromProjectId.includes(obj.parent_project_id) );
+        const filteredArray = allActivityEvents.filter(obj => !obj.extra_data.client?.includes("obsidian")
         //console.log(filteredArray)
         return(filteredArray)
 
