99c148132ddbd1927216d4fc0b9d3c1ea93c8292
HeroBlackInk <drv1@outlook.com>
2024-10-02 16:28:30 +0800

    add utilities.ts

---
+++ src/utilities.ts
@@ -0,0 +1,41 @@
+import { App} from 'obsidian';
+import UltimateTodoistSyncForObsidian from "../main";
+
+
+
+
+export class Utilities   {
+	app:App;
+  plugin: UltimateTodoistSyncForObsidian;
+
+	constructor(app:App) {
+		//super(app,settings);
+		this.app = app;
+ 
+	}
+
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
